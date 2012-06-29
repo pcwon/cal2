51239b11177bc8a78285f2b3dd6a3109ebd71a21
Park Chang Won <Park@gmail.com>
2012-06-29 09:33:48 +0900

    Initial Commit

---
+++ actionGame/abcAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  abcAppDelegate.h
+//  actionGame
+//
+//  Created by lab09 on 12. 6. 29..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface abcAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ actionGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  actionGame
+//
+//  Created by lab09 on 12. 6. 29..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "abcAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([abcAppDelegate class]));
+    }
+}
